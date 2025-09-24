56195189e27c5e211f293753b23341d42afa74d5
trieu051604 <trieuquoc16052004@gmail,com>
2025-09-24 10:26:01 +0700

    done-move-up-down-right

---
+++ Assets/Scripts/PlayMovenments.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayMovenments : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 5f;
+
+    public Animator animator;
+  
+    private Vector2 movement;
+
+       
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
+       
+
+
+
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
