6559276dcb62e3cc98658331a09c2c1012258d7b
xuanhien010204 <221967310+xuanhien010204@users.noreply.github.com>
2025-11-03 18:22:49 +0700

    add game menu, start game , pause and quit game.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,26 +11,31 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Image energryBar;
     [SerializeField] private GameObject gameUi;
 
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject gameOverMenu;
+    [SerializeField] private GameObject pauseMenu;
+
     void Start()
     {
         currentEnergy = 0;
         boss.SetActive(false);
+        MainMenu();
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
     public void AddEnergy()
     {
         if (bossCalled) return;
         currentEnergy += 1;
         UpdateEnergyBar();
-        if (currentEnergy >= energyThreshold )
+        if (currentEnergy >= energyThreshold)
         {
-            CallBoss(); 
+            CallBoss();
         }
     }
     public void CallBoss()
@@ -47,5 +52,40 @@ public class GameManager : MonoBehaviour
             float fillAmount = Mathf.Clamp01((float)currentEnergy / (float)energyThreshold);
             energryBar.fillAmount = fillAmount;
         }
+    }
+    public void MainMenu()
+    {
+        mainMenu.SetActive(true);
+        gameOverMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+        Time.timeScale = 0f;
+    }
+    public void GameOverMenu()
+    {
+        gameOverMenu.SetActive(true);
+        mainMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+        Time.timeScale = 0f;
+    }
+    public void PauseGameMenu()
+    {
+        pauseMenu.SetActive(true);
+        mainMenu.SetActive(false);
+        gameOverMenu.SetActive(false);
+        Time.timeScale = 0f;
+    }
+    public void StartGame()
+    {
+        mainMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+        gameOverMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void ResumeGame()
+    {
+        mainMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+        gameOverMenu.SetActive(false);
+        Time.timeScale = 1f;
     }
 }
